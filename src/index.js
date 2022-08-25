9a6b2302ff735406e0d1b83637c6a0d8009788b1
Semp <tonito5551@gmail.com>
2022-08-25 21:08:21 +0300

    feat: implement switch case for making very-easy pack

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import greenCards from "../data/mythicCards/green/index.js"
 import brownCards from "../data/mythicCards/brown/index.js"
 import blueCards from "../data/mythicCards/blue/index.js"
 
+let draftPack = []
+
+let pack = {
+  firstStage: [],
+  secondStage: [],
+  thirdStage: [],
+}
+
+let dropPack = [];
+
+let difficulty = 'easy';
+
 const createAncients = () => {
   let ancContainer = document.querySelector('.ancients-container');
 
@@ -17,12 +29,25 @@ const createAncients = () => {
 
 createAncients();
 
-const ancObj = document.querySelector('.ancients-container');
-ancObj.onclick=function(element){
-  for(let i = 0; i < ancObj.children.length; i++){
-    ancObj.children[i].classList.remove('card-active');
+let ancientsId;
+
+let ancObj = document.querySelector('.ancients-container');
+
+ancObj.onclick = function(event) {
+  let target = event.target;
+  while (target != this) {
+    highlightAnc(target);
+    return;
   }
-  element.target.classList.add('card-active'); 
+  target = target.parentNode;
+}
+
+function highlightAnc(node) {
+  if (ancientsId) {
+    ancientsId.classList.remove('card-active')
+  }
+  ancientsId = node;
+  ancientsId.classList.add('card-active');
 }
 
 const createDifficulty = () => {
@@ -38,10 +63,106 @@ const createDifficulty = () => {
 
 createDifficulty();
 
+let difficultId;
+
 const diffObj = document.querySelector('.difficulty-container');
-diffObj.onclick=function(element){
-  for(let i = 0; i < diffObj.children.length; i++){
-    diffObj.children[i].classList.remove('difficulty-active');
+
+diffObj.onclick = function(event) {
+  let target = event.target;
+  while (target != this) {
+    highlightDiff(target);
+    return;
   }
-  element.target.classList.add('difficulty-active'); 
+  target = target.parentNode;
 }
+
+function highlightDiff(node) {
+  if (difficultId) {
+    difficultId.classList.remove('difficulty-active')
+  }
+  difficultId = node;
+  difficultId.classList.add('difficulty-active');
+}
+
+const makePack = () => {
+  let brownCount = 0;
+  let blueCount = 0;
+  let greenCount = 0;
+  switch ('very-easy') {
+    case difficulties[0].id:
+      // take green cards in draft
+      for (let i = 0 ; i < greenCards.length; i++) {
+        if (greenCards[i].difficulty === difficulties[1].id) {
+          draftPack.push(greenCards[i])
+        }
+      }
+      // take brown cards in draft
+
+      for (let j = 0; j < brownCards.length; j++) {
+        if (brownCount < 5) {
+          if (brownCards[j].difficulty === difficulties[1].id) {
+            draftPack.push(brownCards[j]);
+            brownCount++;
+          }  
+        } 
+        if (brownCount == 5) {
+          let count = 0;
+          // take additional cards from normal difficulty to draft cause easy cards end
+          for (let k = 0; k < brownCards.length; k++) {
+            if (brownCards[k].difficulty == difficulties[2].id) {
+              if (count < 4) {
+                draftPack.push(brownCards[k]);
+                count++;
+              }
+            }
+          }
+        }
+      }
+      //take blue cards in draft
+      for (let v = 0; v < blueCards.length; v++) {
+        if (blueCount != 2) {
+          if (blueCards[v].difficulty == difficulties[1].id) {
+            draftPack.push(blueCards[v]);       
+            console.log(v)
+            blueCount++;
+          }
+        } if (blueCount == 2) {
+          break
+        }
+        // let pushCount = 0;
+        // console.log(`count before ${pushCount}`)
+        // if (blueCards[v].difficulty == difficulties[1].id) {
+        //   if (pushCount != 2) {
+        //   } else {
+        //     console.log(`count after ${count2}`)
+        //     console.log(`card ${blueCards[v]}`)
+        //     count2++;
+        //     pushCount++;
+        //   }            
+        // }
+      }
+  }
+}
+
+makePack();
+console.log(draftPack)
+// take 5 green cards with diff easy
+// take 9 brown cards with diff easy
+// take 2 blue cards with diff easy
+// if cards not enough you need take normal cards
+// case difficulties[1].id:
+
+// console.log(blueCards.length)
+//console.log(difficulties[0].id)
+//Очень легкий уровень сложности: 
+//из набора берутся все карты со снежинками, если карт не хватает то добираются обычные карты
+//blue - со снежинками
+// if not enough take brown
+
+//сложность вери-изи
+//9 коричневых карт
+//5 коричневых карт (brown) со снежинкой (difficulty:easy)  и добираются обычные
+
+// !
+// ! карты мифов по цвету
+// ! green brown blue
